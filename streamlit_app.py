ef960b03e82534c2b1e46cd1fa5536a0f60ad4cc
Rakshith <avrrakshith@gmail.com>
2023-12-01 14:46:48 +0530

    Updated streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ def convert_numerical_dt(df,column_names):
 
 # Function to fetch data from MySQL
 def fetch_data(query,year,quarter):
-    my_db = sql.connect(host='localhost', user='your_username', password='your_password', database='your_database')
+    my_db = sql.connect(host='localhost', user='root', password='raku#123', database='phonepe_pulse')
     cursor = my_db.cursor()
     cursor.execute(query,(year,quarter))
     column_names = [i[0] for i in cursor.description]
@@ -155,70 +155,185 @@ if SELECT=='Explore Data':
             st.dataframe(df2,hide_index=1)
 
 
-elif options == "Aggregate Users":
-    st.header("Aggregate Users Data")
-    query = """select state, `year`,`quarter`,sum(registered_users) as 'Registered Users',sum(app_opens) as 'App Opens'
-               from aggregate_users where year=%s and quarter=%s
-               group by state, year,quarter"""
-    df = fetch_data(query,year,quarter)
-    state_name = change_state_name()
-    for i in range(len(df.loc[:,'state'])):
-     df['state'][i] = state_name[i]
-    df = convert_numerical_dt(df,'Registered Users')
-    df['App Opens']= df['App Opens'].apply(lambda x: AddUnit.unit(x))
-    col1, col2 = st.columns([0.6, 0.4])
-    with col1:
-        geo_map(df,'Registered Users','App Opens','Aggregate Users Count on India Map')
-    with col2:
-        st.title(" Users ")
-        query1="""select sum(registered_users) as 'Registered Users',sum(app_opens) as 'App Opens'
-                   from aggregate_users where year=%s and quarter=%s"""
-        df1 = fetch_data(query1,year,quarter)
-        st.header(f'Registered PhonePe users till Q{quarter} {year} ')
-        st.subheader(AddUnit.unit(df1['Registered Users'][0])) 
-        st.header(f'PhonePe app opens in Q{quarter} {year}')
-        if df1['App Opens'][0]==0:
-            st.header('Unavailable')
-        else:
-            st.subheader(AddUnit.unit(df1['App Opens'][0]))
-        st.divider()
-        tab1, tab2, tab3 = st.tabs(["State", "District", "Pincode"])
-        with tab1:
-            query2="""select state,sum(registered_users) as 'Registered Users',sum(app_opens) as 'App Opens'
-                  from aggregate_users where year=%s and quarter=%s group by state,year,quarter order by sum(registered_users) desc limit 10"""
-            df2 = fetch_data(query2,year,quarter)
-            df2['Registered Users']=df2['Registered Users'].apply(lambda x:AddUnit.unit(x))
-            df2['App Opens']=df2['App Opens'].apply(lambda x:AddUnit.unit(x))
-            st.dataframe(df2,hide_index=1)
-            with st.expander('See Explanation'):
-                st.write('Data not available for App Opens for year 2018 and for Q1 of 2019')
-        with tab2:
-            query3="""select district,sum(registered_users) as 'Registered Users' from top_users_district where year=%s and quarter=%s
-                      group by year,quarter,district order by sum(registered_users) desc limit 10"""
-            df3=fetch_data(query3,year,quarter)
-            df3['Registered Users']=df3['Registered Users'].apply(lambda x:AddUnit.unit(x))
-            st.dataframe(df3,hide_index=1)
-        with tab3:
-            query4="""select pincode,sum(registered_users) as 'Registered Users' from top_users_pincode where year=%s and quarter=%s
-                      group by year,quarter,pincode order by sum(registered_users) desc limit 10"""
-            df4=fetch_data(query4,year,quarter)
-            st.dataframe(df4,hide_index=1)
-            
+    elif options == 'Aggregate Users':
+        st.header('Aggregate Users Data')
+        query = """select state, `year`,`quarter`,sum(registered_users) as 'Registered Users',sum(app_opens) as 'App Opens'
+                from aggregate_users where year=%s and quarter=%s
+                group by state, year,quarter"""
+        df = fetch_data(query,year,quarter)
+        state_name = change_state_name()
+        for i in range(len(df.loc[:,'state'])):
+            df['state'][i] = state_name[i]
+        df = convert_numerical_dt(df,'Registered Users')
+        df['App Opens']= df['App Opens'].apply(lambda x: AddUnit.unit(x))
+        col1, col2 = st.columns([0.6, 0.4])
+        with col1:
+            geo_map(df,'Registered Users','App Opens','Aggregate Users Count on India Map')
+        with col2:
+            st.title(' Users ')
+            query1="""select sum(registered_users) as 'Registered Users',sum(app_opens) as 'App Opens'
+                    from aggregate_users where year=%s and quarter=%s"""
+            df1 = fetch_data(query1,year,quarter)
+            st.header(f'Registered PhonePe users till Q{quarter} {year} ')
+            st.subheader(AddUnit.unit(df1['Registered Users'][0])) 
+            st.header(f'PhonePe app opens in Q{quarter} {year}')
+            if df1['App Opens'][0]==0:
+                st.header('Unavailable')
+            else:
+                st.subheader(AddUnit.unit(df1['App Opens'][0]))
+            st.divider()
+            tab1, tab2, tab3 = st.tabs(['State', 'District', 'Pincode'])
+            with tab1:
+                query2="""select state,sum(registered_users) as 'Registered Users',sum(app_opens) as 'App Opens', sum(app_opens)/sum(registered_users) as 'Avg App Open by User Per Quarter'
+                    from aggregate_users where year=%s and quarter=%s group by state,year,quarter order by sum(registered_users) desc limit 10"""
+                df2 = fetch_data(query2,year,quarter)
+                df2['Registered Users']=df2['Registered Users'].apply(lambda x:AddUnit.unit(x))
+                df2['App Opens']=df2['App Opens'].apply(lambda x:AddUnit.unit(x))
+                df2 = convert_numerical_dt(df2,'Avg App Open by User Per Quarter')
+                df2['Avg App Open by User Per Quarter']=round(df2['Avg App Open by User Per Quarter'])
+                st.dataframe(df2,hide_index=1)
+                with st.expander('See Explanation'):
+                    st.write('Data not available for App Opens for year 2018 and for Q1 of 2019')
+            with tab2:
+                query3="""select district,sum(registered_users) as 'Registered Users' from top_users_district where year=%s and quarter=%s
+                        group by year,quarter,district order by sum(registered_users) desc limit 10"""
+                df3=fetch_data(query3,year,quarter)
+                df3['Registered Users']=df3['Registered Users'].apply(lambda x:AddUnit.unit(x))
+                st.dataframe(df3,hide_index=1)
+            with tab3:
+                query4="""select pincode,sum(registered_users) as 'Registered Users' from top_users_pincode where year=%s and quarter=%s
+                        group by year,quarter,pincode order by sum(registered_users) desc limit 10"""
+                df4=fetch_data(query4,year,quarter)
+                st.dataframe(df4,hide_index=1)
 
-                    
+if SELECT== 'Insights':
+    options=st.selectbox('Choose an option',['Top 10 Districts by Transaction Amount of each State',
+                                             'Top 10 Districts by Transaction Count of each State',
+                                             'Top 10 Pincodes by Transaction Amount of each State',
+                                             'Top 10 Pincodes by Transaction Count of each State',
+                                             'Transaction Amount of each Transaction Type of each State',
+                                             'Transaction Count of each Transaction Type of each State',
+                                             'Top 10 Districts by Registered Users of each State',
+                                             'Top 10 Pincodes by Registered Users of each State'])
+    if options=='Top 10 Districts by Transaction Amount of each State':
+        col1,col2,col3= st.columns([0.3,0.3,0.3])
+        with col1:
+            state=st.selectbox('Select a State or Union Territory',['andaman-&-nicobar-islands','andhra-pradesh','arunachal-pradesh','assam',
+                                'bihar','chandigarh','chhattisgarh','dadra-&-nagar-haveli-&-daman-&-diu','delhi','goa','gujarat','haryana',
+                                'himachal-pradesh','jammu-&-kashmir','jharkhand','karnataka','kerala','ladakh','lakshadweep','madhya-pradesh',
+                                'maharashtra','manipur','meghalaya','mizoram','nagaland','odisha','puducherry','punjab','rajasthan','sikkim',
+                                'tamil-nadu','telangana','tripura','uttar-pradesh','uttarakhand','west-bengal'])
+        with col2:
+            year=st.selectbox('Select a Year',[2018,2019,2020,2021,2022,2023])
+        with col3:
+            quarter=st.selectbox('Select a Quarter',[1,2,3,4])
+        query="""select state,`year`,`quarter`,district as 'District',transaction_amount as 'Transaction Amount' from top_transaction_district
+                    where state=%s and year=%s and quarter=%s order by transaction_amount"""
+        df=fetch_data_by_state(query,state,year,quarter)
+        fig=px.bar(df,x='District',y='Transaction Amount',color='Transaction Amount',color_continuous_scale='ylorrd')
+        st.plotly_chart(fig,use_container_width=True)
 
-                        
-            
-        
+    elif options=='Top 10 Districts by Transaction Count of each State':
+        col1,col2,col3= st.columns([0.3,0.3,0.3])
+        with col1:
+            state=st.selectbox('Select a State or Union Territory',['andaman-&-nicobar-islands','andhra-pradesh','arunachal-pradesh','assam',
+                                'bihar','chandigarh','chhattisgarh','dadra-&-nagar-haveli-&-daman-&-diu','delhi','goa','gujarat','haryana',
+                                'himachal-pradesh','jammu-&-kashmir','jharkhand','karnataka','kerala','ladakh','lakshadweep','madhya-pradesh',
+                                'maharashtra','manipur','meghalaya','mizoram','nagaland','odisha','puducherry','punjab','rajasthan','sikkim',
+                                'tamil-nadu','telangana','tripura','uttar-pradesh','uttarakhand','west-bengal'])
+        with col2:
+            year=st.selectbox('Select a Year',[2018,2019,2020,2021,2022,2023])
+        with col3:
+            quarter=st.selectbox('Select a Quarter',[1,2,3,4])
+        query="""select state,`year`,`quarter`,district as 'District',transaction_count as 'Transaction Count' from top_transaction_district
+                    where state=%s and year=%s and quarter=%s order by transaction_count"""
+        df=fetch_data_by_state(query,state,year,quarter)
+        fig=px.bar(df,x='District',y='Transaction Count',color='Transaction Count',color_continuous_scale='ylorrd')
+        st.plotly_chart(fig,use_container_width=True)
 
+    elif options=='Top 10 Pincodes by Transaction Amount of each State':
+        col1,col2,col3= st.columns([0.3,0.3,0.3])
+        with col1:
+            state=st.selectbox('Select a State or Union Territory',['andaman-&-nicobar-islands','andhra-pradesh','arunachal-pradesh','assam',
+                                'bihar','chandigarh','chhattisgarh','dadra-&-nagar-haveli-&-daman-&-diu','delhi','goa','gujarat','haryana',
+                                'himachal-pradesh','jammu-&-kashmir','jharkhand','karnataka','kerala','ladakh','lakshadweep','madhya-pradesh',
+                                'maharashtra','manipur','meghalaya','mizoram','nagaland','odisha','puducherry','punjab','rajasthan','sikkim',
+                                'tamil-nadu','telangana','tripura','uttar-pradesh','uttarakhand','west-bengal'])
+        with col2:
+            year=st.selectbox('Select a Year',[2018,2019,2020,2021,2022,2023])
+        with col3:
+            quarter=st.selectbox('Select a Quarter',[1,2,3,4])
+        query="""select state,`year`,`quarter`,pincode as 'Pincode',transaction_amount as 'Transaction Amount' from top_transaction_pincode
+                    where state=%s and year=%s and quarter=%s order by transaction_amount"""
+        df=fetch_data_by_state(query,state,year,quarter)
+        st.bar_chart(df,x='Pincode',y='Transaction Amount',use_container_width=True)
+    
+    elif options=='Top 10 Pincodes by Transaction Count of each State':
+        col1,col2,col3= st.columns([0.3,0.3,0.3])
+        with col1:
+            state=st.selectbox('Select a State or Union Territory',['andaman-&-nicobar-islands','andhra-pradesh','arunachal-pradesh','assam',
+                                'bihar','chandigarh','chhattisgarh','dadra-&-nagar-haveli-&-daman-&-diu','delhi','goa','gujarat','haryana',
+                                'himachal-pradesh','jammu-&-kashmir','jharkhand','karnataka','kerala','ladakh','lakshadweep','madhya-pradesh',
+                                'maharashtra','manipur','meghalaya','mizoram','nagaland','odisha','puducherry','punjab','rajasthan','sikkim',
+                                'tamil-nadu','telangana','tripura','uttar-pradesh','uttarakhand','west-bengal'])
+        with col2:
+            year=st.selectbox('Select a Year',[2018,2019,2020,2021,2022,2023])
+        with col3:
+            quarter=st.selectbox('Select a Quarter',[1,2,3,4])
+        query="""select state,`year`,`quarter`,pincode as 'Pincode',transaction_count as 'Transaction Count' from top_transaction_pincode
+                    where state=%s and year=%s and quarter=%s order by transaction_count"""
+        df=fetch_data_by_state(query,state,year,quarter)
+        st.bar_chart(df,x='Pincode',y='Transaction Count',use_container_width=True)
 
-# elif options == "Map Transaction":
-#     st.header("Map Transaction Data")
-#     query = "SELECT * FROM map_transaction"
-#     df = fetch_data(query)
-#     st.dataframe(df)
+    elif options=='Transaction Amount of each Transaction Type of each State':
+        col1,col2,col3= st.columns([0.3,0.3,0.3])
+        with col1:
+            state=st.selectbox('Select a State or Union Territory',['andaman-&-nicobar-islands','andhra-pradesh','arunachal-pradesh','assam',
+                                'bihar','chandigarh','chhattisgarh','dadra-&-nagar-haveli-&-daman-&-diu','delhi','goa','gujarat','haryana',
+                                'himachal-pradesh','jammu-&-kashmir','jharkhand','karnataka','kerala','ladakh','lakshadweep','madhya-pradesh',
+                                'maharashtra','manipur','meghalaya','mizoram','nagaland','odisha','puducherry','punjab','rajasthan','sikkim',
+                                'tamil-nadu','telangana','tripura','uttar-pradesh','uttarakhand','west-bengal'])
+        with col2:
+            year=st.selectbox('Select a Year',[2018,2019,2020,2021,2022,2023])
+        with col3:
+            quarter=st.selectbox('Select a Quarter',[1,2,3,4])
+        query="""select state,`year`,`quarter`,transaction_type as 'Transaction Type',transaction_amount as 'Transaction Amount'
+          from aggregate_transaction where state=%s and year=%s and quarter=%s order by transaction_amount """
+        df=fetch_data_by_state(query,state,year,quarter)
+        fig=px.bar(df,x='Transaction Type',y='Transaction Amount',color='Transaction Amount',color_continuous_scale='ylorrd')
+        st.plotly_chart(fig,use_container_width=True) 
 
-    # Add your Plotly chart here using df
+    elif options=='Transaction Count of each Transaction Type of each State':
+        col1,col2,col3= st.columns([0.3,0.3,0.3])
+        with col1:
+            state=st.selectbox('Select a State or Union Territory',['andaman-&-nicobar-islands','andhra-pradesh','arunachal-pradesh','assam',
+                                'bihar','chandigarh','chhattisgarh','dadra-&-nagar-haveli-&-daman-&-diu','delhi','goa','gujarat','haryana',
+                                'himachal-pradesh','jammu-&-kashmir','jharkhand','karnataka','kerala','ladakh','lakshadweep','madhya-pradesh',
+                                'maharashtra','manipur','meghalaya','mizoram','nagaland','odisha','puducherry','punjab','rajasthan','sikkim',
+                                'tamil-nadu','telangana','tripura','uttar-pradesh','uttarakhand','west-bengal'])
+        with col2:
+            year=st.selectbox('Select a Year',[2018,2019,2020,2021,2022,2023])
+        with col3:
+            quarter=st.selectbox('Select a Quarter',[1,2,3,4])
+        query="""select state,`year`,`quarter`,transaction_type as 'Transaction Type',transaction_count as 'Transaction Count'
+          from aggregate_transaction where state=%s and year=%s and quarter=%s order by transaction_count """
+        df=fetch_data_by_state(query,state,year,quarter)
+        fig=px.bar(df,x='Transaction Type',y='Transaction Count',color='Transaction Count',color_continuous_scale='ylorrd')
+        st.plotly_chart(fig,use_container_width=True)
 
-# Repeat the above structure for other options
-
+    elif options=='Top 10 Pincodes by Registered Users of each State':
+        col1,col2,col3= st.columns([0.3,0.3,0.3])
+        with col1:
+            state=st.selectbox('Select a State or Union Territory',['andaman-&-nicobar-islands','andhra-pradesh','arunachal-pradesh','assam',
+                                'bihar','chandigarh','chhattisgarh','dadra-&-nagar-haveli-&-daman-&-diu','delhi','goa','gujarat','haryana',
+                                'himachal-pradesh','jammu-&-kashmir','jharkhand','karnataka','kerala','ladakh','lakshadweep','madhya-pradesh',
+                                'maharashtra','manipur','meghalaya','mizoram','nagaland','odisha','puducherry','punjab','rajasthan','sikkim',
+                                'tamil-nadu','telangana','tripura','uttar-pradesh','uttarakhand','west-bengal'])
+        with col2:
+            year=st.selectbox('Select a Year',[2018,2019,2020,2021,2022,2023])
+        with col3:
+            quarter=st.selectbox('Select a Quarter',[1,2,3,4])
+        query="""select state,pincode as 'Pincode',`year`,`quarter`,registered_users as 'Registered Users'
+          from top_users_pincode where state=%s and year=%s and quarter=%s order by registered_users """
+        df=fetch_data_by_state(query,state,year,quarter)
+        st.bar_chart(df,x='Pincode',y='Registered Users',use_container_width=True)
